e8645182526f46e234a7ca57f2bd6c6fd215e574
Haikel Fazzani <haikel.fazzanii@outlook.fr>
2018-03-19 18:28:56 +0100

    Create useful_methods.sql

---
+++ useful_methods.sql
@@ -0,0 +1,5 @@
+Function Name	Examples	                               Return Value
+TO_CHAR ()	    TO_CHAR (3000, '$9999')                $3000
+                TO_CHAR (SYSDATE, 'Day, Month YYYY')   Monday, June 2008
+TO_DATE ()	    TO_DATE ('01-Jun-08')	                 01-Jun-08
+NVL ()	        NVL (null, 1)	                         1
