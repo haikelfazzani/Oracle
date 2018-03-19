cf4ecd0c9dee7c88a9e2f514e0630221acb4a856
Haikel Fazzani <haikel.fazzanii@outlook.fr>
2018-03-19 11:16:47 +0100

    Update CURSOR_EXPLICIT.sql

--- Notes/CURSOR_EXPLICIT.sql
+++ Notes/CURSOR_EXPLICIT.sql
@@ -1,16 +1,23 @@
+cursor v_sql is select id, name from students;
+for rec in v_sql 
+loop
+    -- do anything
+end loop;
+---------------------------------------------------------
+
 v_sql := 'select id, name from students';
 open cur for v_sql;
 for rec in cur loop
     -- do anything
 end loop;
-Or you can do this
+---------------------------------------------------------
 
 cursor cur is select id, name from students;
 open cur;
 for rec in cur loop
         -- do anything
 end loop;
-Or you can do this
+---------------------------------------------------------
 
 for rec in (select id, name from students) loop
     -- do anything
