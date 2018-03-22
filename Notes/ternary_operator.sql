80751231fcf1b1540dcfc212d68c2a3a33979a56
Haikel Fazzani <haikel.fazzanii@outlook.fr>
2018-03-22 21:46:03 +0100

    Update ternary_operator.sql

--- Notes/ternary_operator.sql
+++ Notes/ternary_operator.sql
@@ -3,8 +3,8 @@
 -- DECODE Funciton:
 
 SELECT DECODE(EMP_ID, 1, 'True', 'False') from Employee
-CASE Function:
 
+CASE Function:
 SELECT CASE WHEN EMP_ID = 1 THEN 'True' ELSE 'False' END from Employee
 
 -- Two other alternatives:
@@ -12,6 +12,5 @@ SELECT CASE WHEN EMP_ID = 1 THEN 'True' ELSE 'False' END from Employee
 -- a combination of NULLIF and NVL2. You can only use this if emp_id is NOT NULL, which it is in your case:
 
 select nvl2(nullif(emp_id,1),'False','True') from employee;
-simple CASE expression (Mt. Schneiders used a so-called searched CASE expression)
 
 select case emp_id when 1 then 'True' else 'False' end from employee;
