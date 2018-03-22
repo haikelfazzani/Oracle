2b2bf40f4adfe6bd930cf93259b4b7b009f3681b
Haikel Fazzani <haikel.fazzanii@outlook.fr>
2018-03-22 21:43:39 +0100

    Create ternary_operator.sql

---
+++ Notes/ternary_operator.sql
@@ -0,0 +1,17 @@
+-- Oracle doesn't provide such IIF Function. Instead, try using one of the following alternatives:
+
+-- DECODE Funciton:
+
+SELECT DECODE(EMP_ID, 1, 'True', 'False') from Employee
+CASE Function:
+
+SELECT CASE WHEN EMP_ID = 1 THEN 'True' ELSE 'False' END from Employee
+
+-- Two other alternatives:
+
+-- a combination of NULLIF and NVL2. You can only use this if emp_id is NOT NULL, which it is in your case:
+
+select nvl2(nullif(emp_id,1),'False','True') from employee;
+simple CASE expression (Mt. Schneiders used a so-called searched CASE expression)
+
+select case emp_id when 1 then 'True' else 'False' end from employee;
